66161be0263c5337f25de90f5f93cf6e961a8f74
Kasey Hinton <kaseyjameshinton@gmail.com>
2018-07-12 18:46:00 -0600

    Implements the BookShelf component.

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 /* Components */
-import Book from '../components/book';
+import BookShelf from '../components/bookShelf';
 
 export default class Main extends React.Component {
   render() {
@@ -10,58 +10,30 @@ export default class Main extends React.Component {
         <div className="list-books-title">
           <h1>MyReads</h1>
         </div>
-        <div className="list-books-content">
-          <div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Currently Reading</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                  {this.props.data.books &&
-                    this.props.data.books.length > 0 &&
-                    this.props.data.books
-                      .filter(book => book.shelf === 'currentlyReading')
-                      .map(book => (
-                        <li key={book.id}>
-                          <Book reload={this.props.data.reload} book={book} />
-                        </li>
-                      ))}
-                </ol>
-              </div>
+        {this.props.data.books &&
+          this.props.data.books.length > 0 && (
+            <div className="list-books-content">
+              <BookShelf
+                title="Read"
+                shelf="currentlyReading"
+                books={this.props.data.books}
+                reload={this.props.data.reload}
+              />
+              <BookShelf
+                title="Read"
+                shelf="wantToRead"
+                books={this.props.data.books}
+                reload={this.props.data.reload}
+              />
+              <BookShelf
+                title="Read"
+                shelf="read"
+                books={this.props.data.books}
+                reload={this.props.data.reload}
+              />
             </div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Want to Read</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                  {this.props.data.books &&
-                    this.props.data.books.length > 0 &&
-                    this.props.data.books
-                      .filter(book => book.shelf === 'wantToRead')
-                      .map(book => (
-                        <li key={book.id}>
-                          <Book reload={this.props.data.reload} book={book} />
-                        </li>
-                      ))}
-                </ol>
-              </div>
-            </div>
-            <div className="bookshelf">
-              <h2 className="bookshelf-title">Read</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                  {this.props.data.books &&
-                    this.props.data.books.length > 0 &&
-                    this.props.data.books
-                      .filter(book => book.shelf === 'read')
-                      .map(book => (
-                        <li key={book.id}>
-                          <Book reload={this.props.data.reload} book={book} />
-                        </li>
-                      ))}
-                </ol>
-              </div>
-            </div>
-          </div>
-        </div>
+          )}
+
         <div className="open-search">
           <a href="/search">Add a book</a>
         </div>
