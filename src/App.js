aba8ae9ba94d2a06fcc6931b9d17e71cdd1087b1
Kasey Hinton <kaseyjameshinton@gmail.com>
2018-07-12 18:14:04 -0600

    Removes unused state item.

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ class BooksApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      showSearchPage: false,
       currentlyReadingBooks: [],
       wantToReadBooks: [],
       readBooks: [],
